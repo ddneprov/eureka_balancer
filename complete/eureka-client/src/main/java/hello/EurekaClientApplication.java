1432cb3f88840caf3a5520c714be886b4afd6373
Dneprov <ddneprov_24@mail.ru>
2019-11-14 20:57:52 +0300

    second

--- complete/eureka-client/src/main/java/hello/EurekaClientApplication.java
+++ complete/eureka-client/src/main/java/hello/EurekaClientApplication.java
@@ -6,16 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Scanner;
+
+
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class EurekaClientApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
@@ -27,9 +27,22 @@ class ServiceInstanceRestController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
-    @RequestMapping("/service-instances/{applicationName}")
-    public List<ServiceInstance> serviceInstancesByApplicationName(
-            @PathVariable String applicationName) {
-        return this.discoveryClient.getInstances(applicationName);
+    private static Integer task() {
+        System.out.println(" ============  ВВЕДИ ДАННЫЕ ============= ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextInt() + scanner.nextInt();
+    }
+
+    @RequestMapping(value= "/check", method = RequestMethod.GET)
+    @ResponseBody
+    public Integer getMyData(/*@PathVariable long time*/) {
+        int result = task();
+        return result;
+    }
+
+    //http://10.8.77.22:8889/test
+    @GetMapping(value = "/test")
+    public String test() {
+        return " test 2.0";
     }
 }
